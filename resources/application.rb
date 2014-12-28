c0c2ed51d95e905396e5677dc8cdb9a5cc19a678
Tom Scott <tscott@telvue.com>
2014-12-28 03:56:30 -0500

    Fix attribute calls

--- resources/application.rb
+++ resources/application.rb
@@ -1,20 +1,22 @@
 actions :deploy
 
-attribute :directory, [String], name_attribute: true
-attribute :repo, [String]
-attribute :revision, [String]
-attribute :keep_releases, [Fixnum]
-attribute :rollback_on_error, [TrueClass, FalseClass], default: true
-attribute :services, [Array], default: ['puma']
-attribute :database_adapter, [Symbol], default: :postgresql
-attribute :ruby_version, [String], default: '2.1.5'
-attribute :rails_env, [String], default: 'development'
-attribute :fqdn, [String], default: node.fqdn
-attribute :bundler_args, [String], default: [
+CFG_PATH = '/etc/profile.d/rails.sh'
+
+attribute :directory,          kind_of: [String], name_attribute: true
+attribute :repo,               kind_of: [String]
+attribute :revision,           kind_of: [String]
+attribute :keep_releases,      kind_of: [Fixnum]
+attribute :rollback_on_error,  kind_of: [TrueClass, FalseClass], default: true
+attribute :services,           kind_of: [Array],     default: ['puma']
+attribute :database_adapter,   kind_of: [Symbol], default: :postgresql
+attribute :ruby_version,       kind_of: [String], default: '2.1.5'
+attribute :rails_env,          kind_of: [String], default: 'development'
+attribute :fqdn,               kind_of: [String], default: node.fqdn
+attribute :bundler_args,       kind_of: [String], default: [
   '--deployment',
   '--jobs=4',
   '--without development test'
 ].join("\s")
 
-attribute :credentials, [Hash], default: {}
-attribute :credentials_file_path, [String], default: '/etc/profile.d/rails.sh'
+attribute :credentials,           kind_of: [Hash], default: {}
+attribute :credentials_file_path, kind_of: [String], default: CFG_PATH
